09a29917794cb1f10cfc853c4c697043bb602976
Malmosmo <you@example.com>
2022-08-20 14:10:07 +0200

    minor fix

--- wsgi.py
+++ wsgi.py
@@ -4,9 +4,10 @@ import logging
 import sys
 from pathlib import Path
 
-from main import app as application
 
 BASE_DIR = Path(__file__).resolve().parent
 
 logging.basicConfig(level=logging.DEBUG, filename=BASE_DIR / "logs" / "server.log", format="%(asctime)s %(message)s")
 sys.path.insert(0, str(BASE_DIR))
+
+from main import app as application
