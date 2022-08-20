511438e65b9be79fb7773b37e044c62d4198577b
Malmosmo <you@example.com>
2022-08-20 14:04:23 +0200

    added wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import logging
+import sys
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
+logging.basicConfig(level=logging.DEBUG, filename=BASE_DIR / "logs" / "server.log", format="%(asctime)s %(message)s")
+sys.path.insert(0, str(BASE_DIR))
